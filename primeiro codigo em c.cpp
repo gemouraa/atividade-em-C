543d218526f8e925eef942ed24e1e2ff5d267ca4
gemouraa <160125708+gemouraa@users.noreply.github.com>
2024-02-27 09:34:52 -0300

    Add files via upload

---
+++ primeiro codigo em c.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+	char nome[200];
+	char sexo;
+	int idade;
+	float peso;
+	
+	printf("Digite seu nome:");
+	scanf("%s",&nome);
+	
+	fflush(stdin);
+	
+	printf("Digite sua idade:");
+	scanf("%i",&idade);
+	
+	fflush(stdin);
+	
+	printf("Digite seu sexo, sendo (M) masculino e (F) feminino:");
+	scanf("%c",&sexo);
+	
+	printf("Digite seu peso:");
+	scanf("%f",&peso);
+	
+	
+	printf("=========Exibindo Dados=======\n");
+	printf("Nome: %s \n", nome);
+	printf("Idade: %i \n", idade);
+	printf("sexo: %c \n", sexo);
+	printf("Peso: %f \n", peso);
+}
