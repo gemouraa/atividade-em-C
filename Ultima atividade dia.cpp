ae7f941c44576510ba1bc5692a8861af96447c6e
gemouraa <160125708+gemouraa@users.noreply.github.com>
2024-03-14 11:31:11 -0300

    Add files via upload

---
+++ Ultima atividade dia.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void cabecalho () {
+	system("cls || clear");
+	printf("\n=== SENAI ===\n");
+	fflush(stdin);
+}  
+int main () {
+	setlocale(LC_ALL, "");]
+	float mediaSalario,salario;
+	char sexo;
+	int idade, maiorIdade,menorIdade;
+	cabecalho();
+	printf("Código|\t\tDescrição ")
+	printf("\n  1   |\tAdicionar Pessoas")
+	printf("\n  2   |\tExibir Resultados")
+	printf("\t\t\t\tEscolha:")
+	scanf("%i",&escolha)
+	if (escolha != 1 || escolha != 2 ){
+		printf("Número não reconhecido");
+	while (escolha = 1 || escolha = 2) {
+		printf("Código|\t\tDescrição ");
+	printf("\n  1   |\tAdicionar Pessoas");
+	printf("\n  2   |\tExibir Resultados");
+	printf("\t\t\t\tEscolha:");
+	scanf("%i",&escolha);
+		
+	}
+					
+	}
+	
+	
+	
+
+ 
+	printf("Digite sua idade:");
+	scanf("%i",&idade)
